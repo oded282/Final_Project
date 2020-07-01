f6d539bb8f0b9c52c004685dd77e8bdfd7cfff7a
oded282 <oded282@gmail.com>
2020-07-01 15:26:14 +0300

    Update graph generation

--- generate_graph.py
+++ generate_graph.py
@@ -1,58 +1,8 @@
 import re
 from Final_Project.utils import sort_file
 
-
-def generate_graphviz_old():
-    edge_norm = 2
-    pattern = re.compile(r'[a-z]+\s*[a-z]*')
-    weight_counter = {}
-    dict = {}
-    parg2link = {}
-
-    with open("transsmissions_data", "r", encoding="utf8") as file:
-        line = file.readline()
-        is_link = False
-        while line:
-
-            if is_link:
-                # splited_link_parag = line.split("|")
-                # link = splited_link_parag[1]
-                # paragraph = splited_link_parag[2]
-                # parg2link[paragraph] = link
-                label = "[taillabel=<<table> <tr><td href=\"{}\">link</td></tr> </table>>,label={},penwith={}]"\
-                    .format(line.strip('|').strip('\n'), str(weight_counter[animals]),
-                            weight_counter[animals] / edge_norm)
-                dict[animals] += label
-                is_link = False
-            else:
-                animals_pair = line.split('|')[1]
-                matches = pattern.findall(animals_pair)
-                animals = matches[0].replace(" ", "_") + "->" + matches[1].replace(" ", "_")
-
-                if animals in dict:
-                    link = file.readline()
-                    link = "\n\t<tr><td href=\"{}\">link</td></tr>".format(link.strip('|').strip('\n'))
-                    weight_counter[animals] += 1
-                    index = dict[animals].index('</table>>')
-                    dict[animals] = (dict.get(animals)[:index] + link + "</table>>,label=" + str(weight_counter[animals])
-                                     + ",penwidth=" + str(weight_counter[animals] / edge_norm) + "]")
-                    line = file.readline()
-                    continue
-
-                weight_counter[animals] = 1
-                dict[animals] = animals
-                is_link = True
-
-            line = file.readline()
-
-        with open("data/transmission_graph", "w") as file:
-            file.write("digraph prof {\n" + "\tratio = fill;\n" + "\tnode [style=filled, fillcolor=lightblue];\n")
-            for val in dict.values():
-                file.write("\t"+val + "\n")
-            file.write("}")
-
 def generate_graphviz_data(file_name):
-    edge_norm = 2
+    edge_norm = 5
     pattern = re.compile(r'[a-z]+\s*[a-z]*')
     edges_weights = {}
     animals_dict = {}
@@ -88,7 +38,7 @@ def generate_graphviz_data(file_name):
                 index = animals_dict[animals].index('</table>>')
                 animals_dict[animals] = (animals_dict.get(animals)[:index] + link + "</table>>,label=" +
                                          str(edges_weights[animals]) + ",penwidth=" +
-                                         str(edges_weights[animals] / edge_norm) + "]")
+                                         str((edges_weights[animals] / edge_norm) + 2) + "]")
 
     with open("data/transmission_graph", "w") as file:
         file.write("digraph prof {\n" + "\tratio = fill;\n" + "\tnode [style=filled, fillcolor=lightblue];\n")
