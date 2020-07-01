b741808bd76e321707c01c64b0301086ded3f207
Almog Levi <almog.levi@live.biu.ac.il>
2020-07-01 12:08:37 +0300

    new data

--- filter_queries.py
+++ filter_queries.py
@@ -4,7 +4,7 @@ from pattern3.en import pluralize, singularize
 import re
 
 
-def check_animal_in_sentence(animals_set, data, per_animals_list, sentence, complex_sentence_list, link):
+def check_animal_in_sentence(animals_set, data, per_animals_list, sentence, complex_sentence_list, link,paragraph):
     animals_in_sentence_set = set()
 
     for word in sentence.split(" "):
@@ -21,10 +21,10 @@ def check_animal_in_sentence(animals_set, data, per_animals_list, sentence, comp
         return False, False
     if len(animals_in_sentence_set) == 2:
         per_animals_list.append((animals_in_sentence_set.pop(), animals_in_sentence_set.pop()))
-        data.append(sentence + "|" + link)
+        data.append(sentence + "|" + link + "|" + paragraph)
         return True, True
 
-    complex_sentence_list.append((animals_in_sentence_set, sentence + "|" + link))
+    complex_sentence_list.append((animals_in_sentence_set, sentence + "|" + link + "|" + paragraph))
     return True, False
 
 
@@ -37,23 +37,24 @@ def filter_1_result_query(file_name, animals_set):
     prev_sent = ""
     with open(file_name, "r", encoding="utf8") as file:
         next(file)
+        # data_fix = file.read()
+        # data.replace("", "\"")
         for line in file:
+
             line = line.lower()
             temp = line.split("\t")
+            if len(temp) < 17:
+                continue
+
             link = temp[4]
+            paragraph = temp[17]
             sentence = temp[16]
             # remove dups results.
             if prev_sent == sentence:
                 continue
 
-            is_add_sentence = check_animal_in_sentence(animals_set, data, per_animals_list, sentence,
-                                                       complex_sentence_list,link)
-
-            # if is_add_sentence[0]:
-            #     if is_add_sentence[1]:
-            #         link_to_articl.append(temp[4])
-            #     else:
-            #         link_to_articl_complex_sentence.append(temp[4])
+            check_animal_in_sentence(animals_set, data, per_animals_list, sentence,
+                                                       complex_sentence_list,link,paragraph)
 
             prev_sent = sentence
     data.pop(0)
@@ -75,21 +76,21 @@ def filter_2_result_query(data, per_animals_list):
 
 
 def write_to_file(result, complex_sentence_list):
-    with open("transsmissions_data", "w", encoding="utf8") as file:
+    with open("transsmissions_data3", "w", encoding="utf8") as file:
         for index, line in enumerate(result):
-            file.write(str(index) + "|" + str(line[0]) + "|" + str(line[1]) + '\n')
+            file.write(str(index) + "|" + str(line[0]) + "|" + str(line[1]))
 
         file.write("\n----------------complex sentence--------------------------\n\n")
 
         for index, line in enumerate(complex_sentence_list):
             animals_in_sentence, sentence = line[0], line[1]
-            file.write(str(index) + "|(" + ",".join(animals_in_sentence) + ")|" + sentence + '\n')
+            file.write(str(index) + "|(" + ",".join(animals_in_sentence) + ")|" + sentence)
 
 
 def filter_transmit_query():
     animals_set = arrange_animals_list(animals_list)
     result_after_filter_1, per_animals_list_1, complex_sentence_list = filter_1_result_query(
-        "data/transmit_query_results2.tsv", animals_set)
+        "data/transmit_query_results3.tsv", animals_set)
     print(result_after_filter_1)
     print(per_animals_list_1)
     result = filter_2_result_query(result_after_filter_1, per_animals_list_1)
